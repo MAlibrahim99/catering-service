92c8a17121c1d170ca91db4b480f435e7f64ebda
Mohamad Alibrahim <mohamad.alibrahim@mailbox.tu-dresden.de>
2021-11-05 17:30:50 +0100

    Profilseite geschrieben & AccountController hinzugefügt Einige Mehtoden von CustomerController zu AccountController geschoben

---
+++ src/main/java/kickstart/accountServices/AccountController.java
@@ -0,0 +1,55 @@
+package kickstart.accountServices;
+
+import kickstart.customer.ProfileForm;
+import kickstart.customer.RegistrationFrom;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AccountController {
+	private String name = "Hannes Wurst";
+	private String email = "hannes.wurst@mampf.de";
+	private String password = "123";
+	private String address = "";
+
+	@PostMapping("/register")
+	public String registerNewCustomer(@ModelAttribute("registrationForm") RegistrationFrom form, Model model){
+		model.addAttribute("userName", form.getName());
+		return "main-page";
+	}
+
+	@GetMapping("/register")
+	public String register(Model model, RegistrationFrom form){
+		model.addAttribute("registrationForm", form);
+		return "register";
+	}
+
+	@PostMapping("/login")
+	public String logUserIn(@RequestParam("email")String email,
+							@RequestParam("password") String password,
+							Model model){
+		if(email.equals(this.email) && password.equals(this.password)){
+			model.addAttribute("userName", this.name);
+			return "main-page";
+		}else{
+			return "login-page";
+		}
+	}
+
+	@GetMapping("/login")
+	public String sendLoginForm(){
+		return "login-page";
+	}
+
+	@GetMapping("/profile")
+	public String showCustomerAccount(Model model) {
+		ProfileForm form = new ProfileForm(name, email, address);
+		System.out.println("/profile: " + form);
+		model.addAttribute("accountData", form);
+		return "profile";
+	}
+}
--- src/main/java/kickstart/customer/CustomerController.java
+++ src/main/java/kickstart/customer/CustomerController.java
@@ -1,51 +1,14 @@
 package kickstart.customer;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CustomerController {
-
-	private String name = "Hannes Wurst";
-	private String email = "hannes.wurst@mampf.de";
-	private String password = "123";
-	private String address = "";
-
-	@PostMapping("/register")
-	public String registerNewCustomer(@ModelAttribute("registrationForm") RegistrationFrom form, Model model){
-		model.addAttribute("userName", form.getName());
-		return "main-page";
-	}
-
-	@GetMapping("/register")
-	public String register(Model model, RegistrationFrom form){
-		model.addAttribute("registrationForm", form);
-		return "register";
-	}
-
-	@PostMapping("/login")
-	public String logUserIn(@RequestParam("email")String email,
-							@RequestParam("password") String password,
-							Model model){
-		if(email.equals(this.email) && password.equals(this.password)){
-			model.addAttribute("userName", this.name);
-			return "main-page";
-		}else{
-			return "login-page";
-		}
-	}
-
-	@GetMapping("/login")
-	public String sendLoginForm(){
-		return "login-page";
-	}
 
 	@GetMapping("/customer-list")
 	public String showCustomerList() {
 		return "customer-list";
 	}
+
 }
