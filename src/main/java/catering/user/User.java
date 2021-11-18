ff898121c09b4b14d24a8aa6db7a3662c5bdb808
Mohamad Alibrahim <mohamad.alibrahim@mailbox.tu-dresden.de>
2021-11-18 21:40:22 +0100

    Basisnutzerklasse gesschrieben und Anmelde-Route zu eigener login.html führen

--- src/main/java/catering/user/User.java
+++ src/main/java/catering/user/User.java
@@ -1,4 +1,35 @@
 package catering.user;
 
+import org.salespointframework.useraccount.UserAccount;
+
 public class User {
+	// annotate for DB
+	private long id;
+	private String address;
+
+	private UserAccount userAccount;
+
+	public User() {
+	}
+
+	public User(String address, UserAccount userAccount) {
+		this.address = address;
+		this.userAccount = userAccount;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public UserAccount getUserAccount() {
+		return userAccount;
+	}
 }
