c01c08558fa4230fd94f1ca211fbff17cfacbe92
Mohamad Alibrahim <mohamad.alibrahim@mailbox.tu-dresden.de>
2021-11-20 00:12:25 +0100

    Registrierung-Funktion mit register.html erstellt

--- src/main/java/catering/user/UserController.java
+++ src/main/java/catering/user/UserController.java
@@ -1,11 +1,11 @@
 package catering.user;
 
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 
@@ -22,8 +22,19 @@ public class UserController {
 
 	@PostMapping("/register")
 	public String registerUser(@Valid @ModelAttribute("registrationForm") RegistrationForm form, Model model){
-		model.addAttribute("userName", form.getName());
-//		userManagement.createUser(form);
+		// wenn name oder email bereits besetzt ist, gebe nutzer meldung zurück
+		if(userManagement.usernameAlreadyExists(form.getName())){
+			model.addAttribute("usernameAlreadyExists", true);
+		}
+		if(userManagement.emailAlreadyExists(form.getEmail())){
+			model.addAttribute("emailAddressAlreadyExists", true);
+		}
+		if(model.containsAttribute("usernameAlreadyExists") || model.containsAttribute("emailAddressAlreadyExists")){
+			return "register";
+		}
+
+		userManagement.createUser(form);
+		model.addAttribute("userName", form.getName()); // für Benutzer begrüßen
 		return "welcome";
 	}
 
