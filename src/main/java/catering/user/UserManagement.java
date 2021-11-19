25ec39bda16d69fb40ee66164656f866c1f6f024
Mohamad Alibrahim <mohamad.alibrahim@mailbox.tu-dresden.de>
2021-11-19 18:54:43 +0100

    UserManagement bearbeitet

--- src/main/java/catering/user/UserManagement.java
+++ src/main/java/catering/user/UserManagement.java
@@ -1,4 +1,37 @@
 package catering.user;
 
+import org.salespointframework.useraccount.Password.UnencryptedPassword;
+import org.salespointframework.useraccount.Role;
+import org.salespointframework.useraccount.UserAccount;
+import org.salespointframework.useraccount.UserAccountManagement;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class UserManagement {
+
+	private final UserRepository users;
+	private final UserAccountManagement accountManagement;
+	public static final Role CUSTOMER_ROLE = Role.of("CUSTOMER");
+//	public static final Role STAFF_ROLE = Role.of("STAFF");
+//	public static final Role ADMIN_ROLE = Role.of("ADMIN");
+
+	public UserManagement(UserRepository users, UserAccountManagement accountManagement) {
+		if(users == null || accountManagement == null){
+			throw new IllegalArgumentException("userRepository or userAccountManager can not be assigned to null");
+		}
+		this.users = users;
+		this.accountManagement = accountManagement;
+	}
+
+	public User createUser(RegistrationForm form){
+		if(form == null){
+			throw new IllegalArgumentException("User can not be created with value null of RegistrationForm");
+		}
+		UnencryptedPassword password= UnencryptedPassword.of(form.getPassword());
+		UserAccount userAccount = accountManagement.create(form.getName(), password, CUSTOMER_ROLE);
+		System.out.println("Saving user and user account");
+		return users.save(new User(userAccount, "None"));
+	}
 }
