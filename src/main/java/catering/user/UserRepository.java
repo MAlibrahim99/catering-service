8f695896b21b14e3e769f1e3ff76f276875a630b
Mohamad Alibrahim <mohamad.alibrahim@mailbox.tu-dresden.de>
2021-11-20 13:59:45 +0100

    last State where i tried to figure out how login works

--- src/main/java/catering/user/UserRepository.java
+++ src/main/java/catering/user/UserRepository.java
@@ -1,6 +1,10 @@
 package catering.user;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.util.Streamable;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+	//	User findByUsername(String username);
+//	@Override
+//	Streamable<User> findAll();
 }
