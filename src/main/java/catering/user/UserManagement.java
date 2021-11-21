34f6525661c3bf1b365503c7b450d0065fddaf13
Mohamad Alibrahim <mohamad.alibrahim@mailbox.tu-dresden.de>
2021-11-21 09:56:46 +0100

    Anmeldefunktion implementiert.

--- src/main/java/catering/user/UserManagement.java
+++ src/main/java/catering/user/UserManagement.java
@@ -20,7 +20,7 @@ public class UserManagement {
 	public static final Role STAFF_ROLE = Role.of("STAFF");
 //	public static final Role ADMIN_ROLE = Role.of("ADMIN");
 
-	public UserManagement(UserRepository users, @Qualifier("persistentUserAccountManagement") UserAccountManagement accountManagement) { //TODO ich soll noch UserAccountManagement Qualifizieren
+	public UserManagement(UserRepository users, @Qualifier("persistentUserAccountManagement") UserAccountManagement accountManagement) {
 		if(users == null || accountManagement == null){
 			throw new IllegalArgumentException("userRepository or userAccountManager can not be assigned to null");
 		}
