163bbe6aa2cc4b7663e386947550ce8e1558bd31
Mohamad Alibrahim <mohamad.alibrahim@mailbox.tu-dresden.de>
2021-11-23 03:15:25 +0100

    Profilseite mit Nutzerdaten anzeigen, Personalkonto Erstellungsfunktion implementiert, Registrierung überarbeitet

--- src/main/java/catering/user/UserController.java
+++ src/main/java/catering/user/UserController.java
@@ -1,13 +1,21 @@
 package catering.user;
 
+import org.salespointframework.useraccount.UserAccount;
+import org.salespointframework.useraccount.web.LoggedIn;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
 
 @Controller
 public class UserController {
@@ -23,7 +31,7 @@ public class UserController {
 	@PostMapping("/register")
 	public String registerUser(@Valid @ModelAttribute("registrationForm") RegistrationForm form, Model model){
 		// wenn name oder email bereits besetzt ist, gebe nutzer meldung zurück
-		if(userManagement.usernameAlreadyExists(form.getName())){
+		if(userManagement.usernameAlreadyExists(form.getUsername())){
 			model.addAttribute("usernameAlreadyExists", true);
 		}
 		if(userManagement.emailAlreadyExists(form.getEmail())){
@@ -34,7 +42,7 @@ public class UserController {
 		}
 
 		userManagement.createUser(form);
-		model.addAttribute("userName", form.getName()); // für Benutzer begrüßen
+		model.addAttribute("userName", form.getLastName()); // für Benutzer begrüßen
 		return "welcome";
 	}
 
@@ -44,6 +52,16 @@ public class UserController {
 		return "register";
 	}
 
+	@GetMapping("/profile")
+	public String sendProfilePage(@LoggedIn Optional<UserAccount> account, Model model){
+		System.out.println("Profile info: " + account.isPresent());
+		account.ifPresent(userAccount -> System.out.println(userAccount.getUsername()));
+		if(account.isPresent()){
+			model.addAttribute("user", userManagement.findByUsername(account.get().getUsername()));
+			return "profile";
+		}
+		return "login";
+	}
 
 	@GetMapping("/test")
 	@PreAuthorize("isAuthenticated()")
--- src/main/java/catering/user/UserManagement.java
+++ src/main/java/catering/user/UserManagement.java
@@ -1,13 +1,12 @@
 package catering.user;
 
+import org.salespointframework.useraccount.*;
 import org.salespointframework.useraccount.Password.UnencryptedPassword;
-import org.salespointframework.useraccount.Role;
-import org.salespointframework.useraccount.UserAccount;
-import org.salespointframework.useraccount.UserAccountManagement;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.function.Predicate;
 
 @Service
@@ -28,14 +27,50 @@ public class UserManagement {
 		this.accountManagement = accountManagement;
 	}
 
-	public User createUser(RegistrationForm form){
+	public User createUser(RegistrationForm form, Role ... roles){
 		if(form == null){
 			throw new IllegalArgumentException("User can not be created with value null of RegistrationForm");
 		}
 		UnencryptedPassword password= UnencryptedPassword.of(form.getPassword());
-		UserAccount userAccount = accountManagement.create(form.getName(), password, form.getEmail(), CUSTOMER_ROLE);
-			System.out.println("Saving " + userAccount.toString());
-			return users.save(new User(userAccount, "None"));
+		UserAccount userAccount = accountManagement.create(form.getUsername(), password, form.getEmail(), roles);
+		userAccount.setFirstname(form.getFirstName());
+		userAccount.setLastname(form.getLastName());
+			return users.save(new User(userAccount, "Sie haben noch keine Adresse gegeben"));
+	}
+
+	public User createStaff(RegistrationForm form){
+		if(form == null){
+			throw new IllegalArgumentException("Staff can not be created with value null of RegistrationForm");
+		}
+		return this.createUser(form, STAFF_ROLE);
+	}
+
+	public boolean deleteUserAccount(UserAccountIdentifier id){
+		if(id ==null){
+			throw new NullPointerException("UserAccountIdentifier can not be null");
+		}
+		if(accountManagement.get(id).isPresent()){
+			accountManagement.delete(accountManagement.get(id).get());
+			return true;
+		}
+		return false;
+	}
+
+	public User findByUsername(String id){
+		if(id == null){
+			throw new NullPointerException("Id can not be null");
+		}
+		if(id.isEmpty()){
+			throw new IllegalArgumentException("Id can not be empty");
+		}
+			ArrayList<User> allUsers = (ArrayList<User>) users.findAll();
+			User user;
+			for(User u: allUsers){
+				if(u.getUserAccount().getUsername().equals(id)){
+					return u;
+				}
+			}
+			return null;
 	}
 
 	public boolean usernameAlreadyExists(String username){
