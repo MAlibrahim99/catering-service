98a240e6047f04ca6f67890dc29138b23ae91a4d
RosaSplittgerber <rosa_marie.splittgerber@mailbox.tu-dresden.de>
2021-12-01 20:38:51 +0100

    html-Seiten erstellt & Controller

--- src/main/java/catering/order/OrderController.java
+++ src/main/java/catering/order/OrderController.java
@@ -1,4 +1,19 @@
 package catering.order;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class OrderController {
+	
+	@GetMapping("/income-overview-month")
+	@PreAuthorize(value="hasRole('ADMIN')")
+	public String showIncomeOverviewMonth(){
+		return "income-overview-month";
+	}
+	
+	@GetMapping("/income-overview-year")
+	@PreAuthorize(value="hasRole('ADMIN')")
+	public String showIncomeOverviewYear(){
+		return "income-overview-year";
+	}
 }
