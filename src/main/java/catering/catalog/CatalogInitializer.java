4cbe37ce73b1fbdde39d7130cab82a68f3b10be8
Lennart Obermüller <lennart.obermueller@mailbox.tu-dresden.de>
2021-12-06 18:51:09 +0100

    extended option with price per person count

--- src/main/java/catering/catalog/CatalogInitializer.java
+++ src/main/java/catering/catalog/CatalogInitializer.java
@@ -28,53 +28,34 @@ public class CatalogInitializer implements DataInitializer {
 			return;
 		}
 
-		// Preis pro 1 Person
-		catalog.save(new Option("Buffet", Money.of(13, EURO), OptionType.FOOD, new String[] {"eventcatering"}));
-		// Preis pro 1 Person
-		catalog.save(new Option("Galadinner", Money.of(25, EURO), OptionType.FOOD, new String[] {"eventcatering"}));
+		catalog.save(new Option("Buffet", Money.of(13, EURO), 1, OptionType.FOOD, new String[] {"eventcatering"}));
+		catalog.save(new Option("Galadinner", Money.of(25, EURO), 1, OptionType.FOOD, new String[] {"eventcatering"}));
 
-		// Preis pro 5 Personen
-		catalog.save(new Option("Schinkenplatte", Money.of(20, EURO), OptionType.FOOD, new String[] {"partyservice"}));
-		// Preis pro 3 Personen
-		catalog.save(new Option("Käseplatte", Money.of(12.5, EURO), OptionType.FOOD, new String[] {"partyservice"}));
-		// Preis pro 3 Personen
-		catalog.save(new Option("Eierplatte", Money.of(14, EURO), OptionType.FOOD, new String[] {"partyservice"}));
-		// Preis pro 6 Personen
-		catalog.save(new Option("Fischplatte", Money.of(32, EURO), OptionType.FOOD, new String[] {"partyservice"}));
-		// Preis pro 4 Personen
-		catalog.save(new Option("Obstplatte", Money.of(25, EURO), OptionType.FOOD, new String[] {"partyservice"}));
-		// Preis pro 4 Personen
-		catalog.save(new Option("Salatplatte", Money.of(25, EURO), OptionType.FOOD, new String[] {"partyservice"}));
-		// Preis pro 10 Personen
-		catalog.save(new Option("Sushi-Abend", Money.of(150, EURO), OptionType.FOOD, new String[] {"partyservice"}));
-		// Preis pro 10 Personen
-		catalog.save(new Option("Pizza-Runde", Money.of(110, EURO), OptionType.FOOD, new String[] {"partyservice"}));
-		// Preis pro 10 Personen
-		catalog.save(new Option("Meeresfrüchte-Menü", Money.of(170, EURO), OptionType.FOOD, new String[] {"partyservice"}));
-		// Preis pro 10 Personen
-		catalog.save(new Option("Torten-Auswahl", Money.of(120, EURO), OptionType.FOOD, new String[] {"partyservice"}));
+		catalog.save(new Option("Schinkenplatte", Money.of(20, EURO), 5, OptionType.FOOD, new String[] {"partyservice"}));
+		catalog.save(new Option("Käseplatte", Money.of(12.5, EURO), 3, OptionType.FOOD, new String[] {"partyservice"}));
+		catalog.save(new Option("Eierplatte", Money.of(14, EURO), 3, OptionType.FOOD, new String[] {"partyservice"}));
+		catalog.save(new Option("Fischplatte", Money.of(32, EURO), 6, OptionType.FOOD, new String[] {"partyservice"}));
+		catalog.save(new Option("Obstplatte", Money.of(25, EURO), 4, OptionType.FOOD, new String[] {"partyservice"}));
+		catalog.save(new Option("Salatplatte", Money.of(25, EURO), 4, OptionType.FOOD, new String[] {"partyservice"}));
+		catalog.save(new Option("Sushi-Abend", Money.of(150, EURO), 10, OptionType.FOOD, new String[] {"partyservice"}));
+		catalog.save(new Option("Pizza-Runde", Money.of(110, EURO), 10, OptionType.FOOD, new String[] {"partyservice"}));
+		catalog.save(new Option("Meeresfrüchte-Menü", Money.of(170, EURO), 10, OptionType.FOOD, new String[] {"partyservice"}));
+		catalog.save(new Option("Torten-Auswahl", Money.of(120, EURO), 10, OptionType.FOOD, new String[] {"partyservice"}));
 
-		// Preis pro 1 Person
-		catalog.save(new Option("Alkoholfreie Getränke", Money.of(9, EURO), OptionType.FOOD, new String[] {"eventcatering",
+		catalog.save(new Option("Alkoholfreie Getränke", Money.of(9, EURO), 1, OptionType.FOOD, new String[] {"eventcatering",
 				"partyservice", "mobilebreakfast"}));
-		// Preis pro 1 Person
-		catalog.save(new Option("Alkoholische Getränke", Money.of(14, EURO), OptionType.FOOD, new String[] {"eventcatering",
+		catalog.save(new Option("Alkoholische Getränke", Money.of(14, EURO), 1, OptionType.FOOD, new String[] {"eventcatering",
 				"partyservice"}));
 
-		// Preis pro 1 Person
-		catalog.save(new Option("Blumen", Money.of(9, EURO), OptionType.GOODS, new String[] {"eventcatering", "rentacook"}));
-		// Preis pro 1 Person
-		catalog.save(new Option("Servietten", Money.of(0.75, EURO), OptionType.GOODS, new String[] {"eventcatering",
+		catalog.save(new Option("Blumen", Money.of(9, EURO), 1, OptionType.GOODS, new String[] {"eventcatering", "rentacook"}));
+		catalog.save(new Option("Servietten", Money.of(0.75, EURO), 1, OptionType.GOODS, new String[] {"eventcatering",
 				"partyservice", "mobilebreakfast", "rentacook"}));
 
-		// Preis pro 1 Person
-		catalog.save(new Option("Dekoration", Money.of(4, EURO), OptionType.EQUIP, new String[] {"eventcatering",
+		catalog.save(new Option("Dekoration", Money.of(4, EURO), 1, OptionType.EQUIP, new String[] {"eventcatering",
 				"rentacook"}));
-		// Preis pro 1 Person
-		catalog.save(new Option("Geschirr", Money.of(8, EURO), OptionType.EQUIP, new String[] {"eventcatering",
+		catalog.save(new Option("Geschirr", Money.of(8, EURO), 1, OptionType.EQUIP, new String[] {"eventcatering",
 				"partyservice", "mobilebreakfast", "rentacook"}));
-		// Preis pro 6 Personen
-		catalog.save(new Option("Tischtücher", Money.of(25, EURO), OptionType.EQUIP, new String[] {"eventcatering",
+		catalog.save(new Option("Tischtücher", Money.of(25, EURO), 6, OptionType.EQUIP, new String[] {"eventcatering",
 				"rentacook"}));
 	}
 }
--- src/main/java/catering/catalog/Option.java
+++ src/main/java/catering/catalog/Option.java
@@ -9,16 +9,18 @@ import org.javamoney.moneta.Money;
 @Entity
 public class Option extends Product {
 
+	private int personCount;
 	private OptionType type;
 
 	@SuppressWarnings({"unused", "deprecation"})
 	private Option() {
 	}
 
-	public Option(String name, Money price, OptionType type, String[] services) {
+	public Option(String name, Money price, int personCount, OptionType type, String[] services) {
 
 		super(name, price);
 
+		this.personCount = personCount;
 		this.type = type;
 
 		for (String s : services) {
@@ -28,5 +30,9 @@ public class Option extends Product {
 
 	public OptionType getType() {
 		return type;
+	}
+
+	public int getPersonCount() {
+		return personCount;
 	}
 }
