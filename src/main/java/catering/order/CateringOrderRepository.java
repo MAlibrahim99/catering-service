f845086494ebc66d251efdc9bc4186c904672327
Mohamad Alibrahim <mohamad.alibrahim@mailbox.tu-dresden.de>
2021-12-08 11:54:28 +0100

    - Einnahemdesamt im Datumintervall anzeigen - Bestellungen nach Status, im Prozentsatz anzeigen - security zur nav hinzugefügt

--- src/main/java/catering/order/CateringOrderRepository.java
+++ src/main/java/catering/order/CateringOrderRepository.java
@@ -12,16 +12,18 @@ import org.springframework.data.util.Streamable;
 import java.time.LocalDateTime;
 
 
-public interface CateringOrderRepository extends JpaRepository<CateringOrder, OrderIdentifier> {
+public interface CateringOrderRepository extends Repository<CateringOrder, OrderIdentifier> {
 
 	Streamable<CateringOrder> findByCompletionDate(LocalDateTime completionDate);
 
 	Streamable<CateringOrder> findByAddress(String address);
-//
-////	@Query(value="SELECT o FROM CateringOrder o WHERE o.completionDate <= startDate AND o.completionDate <= endDate")
-//	@Query(value="select o from CateringOrder o where  o.completionDate between start and endDate ")
+
 	Streamable<CateringOrder> findByCompletionDateBetween(LocalDateTime start, LocalDateTime end);
 
 	Streamable<CateringOrder> findByOrderStatusAndCompletionDateBetween(OrderStatus status, LocalDateTime start,
 																		LocalDateTime end);
+
+	int countByOrderStatusAndCompletionDateBetween(OrderStatus status, LocalDateTime start, LocalDateTime end);
+
+	int countByCompletionDateBetween(LocalDateTime start, LocalDateTime end);
 }
--- src/main/java/catering/user/admin/IncomeOverviewController.java
+++ src/main/java/catering/user/admin/IncomeOverviewController.java
@@ -1,44 +1,56 @@
 package catering.user.admin;
 
-import catering.order.CateringOrder;
 import catering.order.CateringOrderRepository;
-import org.javamoney.moneta.Money;
-import org.springframework.data.util.Streamable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.text.ParseException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Map;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
-import static org.salespointframework.core.Currencies.EURO;
-
 @Controller
-//@PreAuthorize(value="hasRole('ADMIN')")
+@PreAuthorize(value="hasRole('ADMIN')")
 public class IncomeOverviewController {
+
 	CateringOrderRepository orderRepository;
 	private LocalDateTime currentDate = LocalDateTime.now();
+	private IncomeOverview incomeOverview;
 
-	public IncomeOverviewController(CateringOrderRepository orderRepository) {
+
+	public IncomeOverviewController(CateringOrderRepository orderRepository, IncomeOverview incomeOverview) {
 		this.orderRepository = orderRepository;
+		this.incomeOverview = incomeOverview;
 	}
 
 	// Initial wird eine Übersicht der letzten 30 Tage zurückgegeben, exeklusive des aktuellen Tages
 	@GetMapping("/income-overview")
-	public String displayIncomeOverview(@RequestParam("start") Optional<LocalDateTime> start,
-										@RequestParam("end") Optional<LocalDateTime> end, Model model) {
-		final long PAST_DAYS = 30L;
-
-		if (start.isPresent() && end.isPresent()) {
-			System.out.println(orderRepository.findByCompletionDateBetween(start.get(), end.get().minusDays()).toList().size());
-			model.addAttribute("startDate", start);
-			model.addAttribute("endDate", end);
-		}else{
-			System.out.println(orderRepository.findByCompletionDateBetween(currentDate.minusDays(PAST_DAYS) ,
-					currentDate).toList().size());
+	public String displayIncomeOverview(@RequestParam("startDate") Optional<String> startDate,
+										@RequestParam("endDate") Optional<String> endDate, Model model) {
+		LocalDateTime start;
+		LocalDateTime end;
+		if (startDate.isEmpty() || endDate.isEmpty()) {
+			start = LocalDateTime.now().minusDays(30L);
+			end = LocalDateTime.now().minusDays(1L);
+		} else {
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+			start = LocalDate.parse(startDate.get(), formatter).atStartOfDay();
+			end = LocalDate.parse(endDate.get(), formatter).atStartOfDay();
 		}
+
+		if (start.isAfter(end)) {
+			start = end.minusDays(30L);
+		}
+
+		model.addAttribute("totalIncome", incomeOverview.totalIncome(start, end));
+		model.addAttribute("statusPercentages", incomeOverview.statusPercentages(start, end));
+		model.addAttribute("start", start.toLocalDate());
+		model.addAttribute("end", end.toLocalDate());
+
 		return "income-overview";
 	}
 
