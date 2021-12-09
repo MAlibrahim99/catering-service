33380305be9cfe5148985c511c02c2e96748d7fa
Lisa-Marie Müller <lisa-marie_xd@outlook.de>
2021-12-09 14:11:51 +0100

    test

--- src/test/java/catering/catalog/CatalogControllerIntegrationTests.java
+++ src/test/java/catering/catalog/CatalogControllerIntegrationTests.java
@@ -1,9 +1,9 @@
 package catering.catalog;
 
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +16,21 @@ import org.springframework.test.web.servlet.MockMvc;
 class CatalogControllerIntegrationTests {
 	@Autowired
 	MockMvc mvc;
+	@Autowired CatalogController controller;
 
 	@Test
 	void showsHeadline() throws Exception {
 		mvc.perform(get("/eventcatering")) //
 				.andExpect(status().isOk())
 				.andExpect(content().string(containsString("Eventcatering")));
+	}
+
+	@Test
+	void sampleMvcIntegrationTest() throws Exception {
+
+		mvc.perform(get("/mobilebreakfast")). //
+				andExpect(status().isOk()).//
+				andExpect(model().attribute("catalog", is(not(emptyIterable()))));
 	}
 
 }
