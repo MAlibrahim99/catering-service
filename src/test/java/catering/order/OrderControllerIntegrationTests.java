4fc3ae9466b42c541bf1382697b58c279ea7750f
Lennart Obermüller <lennart.obermueller@mailbox.tu-dresden.de>
2021-12-19 16:17:08 +0100

    added getOrderForm test

---
+++ src/test/java/catering/order/OrderControllerIntegrationTests.java
@@ -0,0 +1,66 @@
+package catering.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class OrderControllerIntegrationTests {
+
+	@Autowired
+	OrderController controller;
+
+	@Test
+	@WithMockUser(roles = "CUSTOMER")
+	public void getCorrectOrderForm() {
+
+		Model model = new ExtendedModelMap();
+
+		String htmlReturn = controller.getOrderForm("eventcatering", model, new CateringOrder());
+		assertThat(htmlReturn).isEqualTo("order_form");
+
+		OrderForm form = (OrderForm) model.asMap().get("form");
+
+		assertThat(form.getService()).isEqualTo("eventcatering");
+		assertThat(form.getFoodList()).hasSize(4);
+		assertThat(form.getEquipList()).hasSize(5);
+
+		model = new ExtendedModelMap();
+
+		htmlReturn = controller.getOrderForm("partyservice", model, new CateringOrder());
+		assertThat(htmlReturn).isEqualTo("order_form");
+
+		form = (OrderForm) model.asMap().get("form");
+
+		assertThat(form.getService()).isEqualTo("partyservice");
+		assertThat(form.getFoodList()).hasSize(12);
+		assertThat(form.getEquipList()).hasSize(2);
+
+		model = new ExtendedModelMap();
+
+		htmlReturn = controller.getOrderForm("mobilebreakfast", model, new CateringOrder());
+		assertThat(htmlReturn).isEqualTo("order_form");
+
+		form = (OrderForm) model.asMap().get("form");
+
+		assertThat(form.getService()).isEqualTo("mobilebreakfast");
+		assertThat(form.getFoodList()).hasSize(2);
+		assertThat(form.getEquipList()).hasSize(2);
+
+		model = new ExtendedModelMap();
+
+		htmlReturn = controller.getOrderForm("rentacook", model, new CateringOrder());
+		assertThat(htmlReturn).isEqualTo("order_form");
+
+		form = (OrderForm) model.asMap().get("form");
+
+		assertThat(form.getService()).isEqualTo("rentacook");
+		assertThat(form.getFoodList()).hasSize(0);
+		assertThat(form.getEquipList()).hasSize(5);
+	}
+}
